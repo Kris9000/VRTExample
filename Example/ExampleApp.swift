e2c0aaaee162d2391e465e92433a7aed57d88862
Kris <kris.boonefaes@gmail.com>
2021-07-22 19:41:06 +0200

    leftovers

--- Example/ExampleApp.swift
+++ Example/ExampleApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct ExampleApp: App {
     var body: some Scene {
         WindowGroup {
+            // Disclaimer: we do **not** write production code like the line below ;)
             ListView().environmentObject(ProgramListViewModel(service: ProgramsService()))
         }
     }
--- Example/Models/Program.swift
+++ Example/Models/Program.swift
@@ -3,11 +3,4 @@ import Foundation
 enum Program {
     case broadcast(String, URL)
     case stream(String, URL)
-    
-    enum Key: CodingKey {
-        case type
-        case name
-        case url
-    }
-    
 }
