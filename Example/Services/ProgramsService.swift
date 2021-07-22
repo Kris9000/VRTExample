257519fbd4303057b9c0697281025a534c4b31dc
Kris9000 <kris.boonefaes@gmail.com>
2021-07-22 20:35:32 +0200

    Update ProgramsService.swift

--- Example/Services/ProgramsService.swift
+++ Example/Services/ProgramsService.swift
@@ -5,7 +5,7 @@ struct ProgramsService {
 
     func getPrograms() -> AnyPublisher<ProgramList?, APIError> {
         // TODO: Use a token from the TokenAPI in this call
-        // TODO: The fetchPrograms call will pseudorandomly fail. If this happens, retry up to 10 times
+        // TODO: The fetchPrograms call will pseudorandomly fail. If this happens, retry up to 3 times
         return MockAPI().fetchPrograms(token: nil).eraseToAnyPublisher()
     }
     
